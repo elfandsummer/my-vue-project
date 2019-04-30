cfb2ef62d1b152829b62edaa25777d44f6da7218
xiatw <xiatw@primeton.com>
2019-04-30 16:22:34 +0800

    add sass for this,need to add sass-loader node-sass,and some time need to run npm rebuild node-sass,to reset node-sass because cnpm may get somthing wrong,the config had added the loader so there's nothing else need to do

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './style/index.scss'
+
 import App from './App'
 import router from './router'
 
